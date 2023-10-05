2552f2875f87a8a57aae264a7cf0d2888dceb9e0
van der Stelt <frank.vanderstelt@cgi.com>
2023-10-05 13:42:00 +0200

    Updating, new components

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,13 @@
+import CategoryItem from '../category-item/category-item.component';
+import './directory.styles.scss';
+
+const Directory = ({ categories }) => {
+  return (
+    <div className='categories-container'>
+      {categories.map(({ id, title, imageUrl }) => {
+        return <CategoryItem key={id} title={title} imageUrl={imageUrl} />;
+      })}
+    </div>
+  );
+};
+export default Directory;
