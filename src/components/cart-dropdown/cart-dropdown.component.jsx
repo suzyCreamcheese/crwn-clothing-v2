cd2a5d8822f49d705669d71714734a30d49d4e24
van der Stelt <frank.vanderstelt@cgi.com>
2023-10-10 14:22:25 +0200

    created checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-
+import { useNavigate } from 'react-router';
 import { CartContext } from '../../contexts/cart.context';
 
 import Button from '../button/button.component';
@@ -9,6 +9,12 @@ import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate('/checkout');
+  };
+
   return (
     <div className='cart-dropdown-container'>
       <div className='cart-items'>
@@ -16,7 +22,7 @@ const CartDropdown = () => {
           return <CartItem key={cartItem.id} cartItem={cartItem}></CartItem>;
         })}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 };
